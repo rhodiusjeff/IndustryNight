fccb1fd544eb0a87fc6803bce6841842914035ca
Jeff Simpson <jeff@rhodiuslabs.com>
2026-03-25 22:50:33 -0400

    chore(B0): ignore and remove Next build artifacts

--- packages/react-admin/.next/static/chunks/app/(dashboard)/users/page-de8ae8a4d07745f3.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[297,100,302,932,40,414,345,606,724,594],{3521:function(){}},function(n){n.O(0,[971,117,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
